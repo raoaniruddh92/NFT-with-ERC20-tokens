c8d815d5023904d5ef57eca52f79faf74fef10d9
Aniruddh Rao <raoaniruddh92@gmail.com>
2025-11-25 22:38:40 +0530

    Update Pet.sol

--- backend/src/Pet.sol
+++ backend/src/Pet.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
+contract Mypet is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
 constructor(address initialOwner)
         ERC721("MyPET", "MPT")
         Ownable(initialOwner)
